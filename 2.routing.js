95ff47c9d6134b3c7e00c2bb89f6f9518b185ba7
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-15 13:23:10 -0600

    making POST request with Rest API

--- 2.routing.js
+++ 2.routing.js
@@ -19,7 +19,24 @@ const processRequest = (req, res) => {
             switch(url){
                 case '/pokemon':{
                     let body = '';
-                }
+
+                    // el body llega por trozos o porciones (chunks) en binario y con toString lo convertimos a texto
+                    req.on('data', (chunk) => {
+                        body += chunk.toString();
+                    });
+                    // node js es basado en eventos, por lo que el evento end se dispara cuando se termina de recibir la data del body
+                    req.on('end', () => {
+                        const parsedBody = JSON.parse(body);
+                        res.writeHead(201, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify(parsedBody));
+                    });
+                    break;
+                };
+                default:
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                    res.end('Ruta no encontrada');
+                
                     
             }
     }
