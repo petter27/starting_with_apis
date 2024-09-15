edd99b9406b23595bb05cbb5203e22574af9287e
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-15 12:19:49 -0600

    installing nodemon
    watch live changes in any files, configure a dev command.

--- 1.http.js
+++ 1.http.js
@@ -2,14 +2,29 @@ const http = require('node:http');
 
 const desiredPort = process.env.PORT ?? 3000;
 
-const server = http.createServer((req, res) => {
-  console.log('Petición recibida');
-  res.end('Hola mundo');
-});
+const processRequest = (req, res) => {
+  
+    if(req.url === '/'){
+        res.statusCode = 200; // OK
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('Bienvenido a la página principal');
+
+    }else if (req.url === '/saludo'){
+        res.statusCode = 200; // OK
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('Hola mundo');
+    }else{
+        res.statusCode = 404; // Not Found
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('Ruta no encontrada');
+    }
+};
+
+const server = http.createServer(processRequest);
 
 // comodin puerto = 0 para que el sistema operativo asigne un puerto disponible automáticamente
 server.listen(desiredPort, () => {
   console.log(
-    `Servidor escuchando en el puerto http://localhost:${port}`
+    `Servidor escuchando en el puerto http://localhost:${desiredPort}`
   );
 });
