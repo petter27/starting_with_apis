cd05dd88f8529ac92a0ffd98a97b90d86d11348b
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-15 11:27:43 -0600

    iniciando proyecto node

--- 1.http.js
+++ 1.http.js
@@ -1,5 +1,4 @@
 const http = require('node:http');
-const { findAvailablePort } = require('./14.free-port.js');
 
 const desiredPort = process.env.PORT ?? 3000;
 
@@ -7,13 +6,10 @@ const server = http.createServer((req, res) => {
   console.log('Petición recibida');
   res.end('Hola mundo');
 });
+
 // comodin puerto = 0 para que el sistema operativo asigne un puerto disponible automáticamente
-findAvailablePort(desiredPort).then(port => {
-  server.listen(port, () => {
-    console.log(
-      `Servidor escuchando en el puerto http://localhost:${server.address().port}`
-    );
-  });
-}).catch(error => {
-  console.error('Error al encontrar puerto disponible:', error);
+server.listen(desiredPort, () => {
+  console.log(
+    `Servidor escuchando en el puerto http://localhost:${port}`
+  );
 });
